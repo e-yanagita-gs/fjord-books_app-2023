dd4ae7bf97f2ce8daa84e7e45b53c8d56b181bf9
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-18 00:58:17 +0900

    デフォルトの表示言語を英語から日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module BooksApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = %i(ja en)
     config.i18n.enforce_available_locales = true
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
