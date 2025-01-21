4e4b87fb9f043050eff588e96adc987a6d9456a3
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-21 17:44:09 +0900

    ログアウトボタンの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'books#index'
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'home#index'
 end
