e4ae94c1f1ce3016638a5de93f4f36d0133a47b3
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-22 16:00:20 +0900

    ユーザー編集画面とログアウトボタンの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:post_code, :address, :bio])
+  end
+
+  def set_user
+    @user = current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy    
+  has_many :books, dependent: :destroy
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
