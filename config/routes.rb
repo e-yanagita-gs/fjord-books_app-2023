632311a4aa619538948f8154a334fdd36836c959
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-24 11:04:58 +0900

    letter_opener_webの使用

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
