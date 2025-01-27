14eba933f696aed28a556baab65a06af19d9e974
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-27 19:56:17 +0900

    rubocop適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:post_code, :address, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[post_code address bio])
   end
 
   def set_user
