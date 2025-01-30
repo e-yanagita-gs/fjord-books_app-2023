a57618cfac8645332b57843f2ac494f02aee182b
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-30 18:09:27 +0900

    アップロード可能ファイルの制限及びプロフィール編集画面にもアイコンを表示するよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  validate :avatar_type
+
+  private
+
+  def avatar_type
+    return unless avatar.attached? && !avatar.blob.content_type.in?(%('image/jpeg image/png image/gif'))
+
+    avatar.purge
+    errors.add(User.human_attribute_name(:avatar), 'はjpegまたはpngまたはgif形式でアップロードしてください')
+  end
 end
