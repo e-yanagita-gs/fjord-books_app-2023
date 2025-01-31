c1044147d77694116ee4b3d679cd7764eef06527
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-31 09:39:49 +0900

    不要なメソッド(set_user)の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_user
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[post_code address bio])
-  end
-
-  def set_user
-    @user = current_user
   end
 end
