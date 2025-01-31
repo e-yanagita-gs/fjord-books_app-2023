063a79c7d1fd5d9f216298b97e6164855259c100
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-01-31 17:52:38 +0900

    日報の更新・削除の権限を投稿者のみに制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_31_050645) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_31_084527) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_31_050645) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
