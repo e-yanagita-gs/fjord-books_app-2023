86af0faf872acffd7de08e5c3d70795639be5f18
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-05 15:55:31 +0900

    日報とメンションの多対多関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions, class_name: 'Mention', foreign_key: 'mentioner_id', dependent: :destroy
+  has_many :mentioning_reports, through: :active_mentions, source: :mentionee
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'mentionee_id', dependent: :destroy
+  has_many :mentioned_reports, through: :passive_mentions, source: :mentioner
 
   validates :title, presence: true
   validates :content, presence: true
