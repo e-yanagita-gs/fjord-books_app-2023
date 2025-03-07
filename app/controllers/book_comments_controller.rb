2915e5074f11d3cefb728a40f5cd503e8b188985
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-07 14:25:08 +0900

    Comments_conrollerを親クラスに設定し、重複したコードをまとめる

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,31 +1,13 @@
 # frozen_string_literal: true
 
-class BookCommentsController < ApplicationController
-  before_action :set_book
-
-  def create
-    @comment = @book.comments.build(comment_params)
-    @comment.user = current_user
-    if @comment.save
-      redirect_to @book, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      flash.now[:alert] = t('views.common.error')
-      render 'books/show'
-    end
-  end
-
-  def destroy
-    @book.comments.find(params[:id]).destroy!
-    redirect_to @book
-  end
-
+class BookCommentsController < CommentsController
   private
 
-  def set_book
-    @book = Book.find(params[:book_id])
+  def set_resource
+    @commentable = Book.find(params[:book_id])
   end
 
-  def comment_params
-    params.require(:comment).permit(:content)
+  def view_path
+    'books'
   end
 end
--- app/controllers/report_comments_controller.rb
+++ app/controllers/report_comments_controller.rb
@@ -1,31 +1,13 @@
 # frozen_string_literal: true
 
-class ReportCommentsController < ApplicationController
-  before_action :set_report
-
-  def create
-    @comment = @report.comments.build(comment_params)
-    @comment.user = current_user
-    if @comment.save
-      redirect_to @report, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      flash.now[:alert] = t('views.common.error')
-      render 'reports/show'
-    end
-  end
-
-  def destroy
-    @report.comments.find(params[:id]).destroy!
-    redirect_to @report
-  end
-
+class ReportCommentsController < CommentsController
   private
 
-  def set_report
-    @report = Report.find(params[:report_id])
+  def set_resource
+    @commentable = Report.find(params[:report_id])
   end
 
-  def comment_params
-    params.require(:comment).permit(:content)
+  def view_path
+    'reports'
   end
 end
