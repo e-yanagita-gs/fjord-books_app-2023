a4677ebf366ec1ba68787308dd8004fb543bd3d2
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-14 10:15:23 +0900

    fixtures :allの再適用及び再適用時にエラーが出ないようymlファイルを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,11 +3,5 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test 'editable?' do
-    user = User.new(name: 'Alice')
-    report = Report.new(user: user)
 
-    target_user = report.user
-    assert report.editable?(user)
-  end
 end
