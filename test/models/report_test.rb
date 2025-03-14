89f57b027545f3326751fc3b14af2da6c75431f3
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-14 10:03:16 +0900

    Reportモデルのテストを追加(fixtures未使用)

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable?' do
+    user = User.new(name: 'Alice')
+    report = Report.new(user: user)
+
+    target_user = report.user
+    assert report.editable?(user)
+  end
 end
