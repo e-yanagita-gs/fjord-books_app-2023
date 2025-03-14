250930a43abc9f0d455d3e6e7da539826cd2511c
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-14 18:50:50 +0900

    不要なrestore_avatarメソッドの削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,23 +3,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :store_current_avatar, only: [:update]
 
-  def update
-    super do |user|
-      restore_avatar(user) if user.errors.any?
-    end
-  end
-
   private
 
   def store_current_avatar
     return unless current_user.avatar.attached?
 
-    @original_avatar_blob = current_user.avatar.blob
-  end
-
-  def restore_avatar(user)
-    return unless @original_avatar_blob && !user.avatar.attached?
-
-    user.avatar.attach(@original_avatar_blob)
+    @original_avatar = current_user.avatar
   end
 end
