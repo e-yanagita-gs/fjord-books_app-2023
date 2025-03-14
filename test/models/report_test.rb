5efa5c68a9a3d55748aeff87e95320875d51594c
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-14 10:57:59 +0900

    Reportモデルのテストの追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,5 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @alice_report = reports(:alice_report)
+  end
 
+  test 'editable?' do
+    assert_equal true, @alice_report.editable?(@alice)
+    assert_equal false, @alice_report.editable?(@bob)
+  end
 end
