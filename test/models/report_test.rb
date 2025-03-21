64d6c0fb2f1abc8c9e47b4221a401205a3a40705
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-21 10:20:52 +0900

    save_mentionsメソッドのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -17,4 +17,11 @@ class ReportTest < ActiveSupport::TestCase
   test 'created_on' do
     assert_equal Time.zone.today, @alice_report.created_on
   end
+
+  test 'save_mentions' do
+    bob_report = @bob.reports.build(title: 'MentioningReport', content: "http://localhost:3000/reports/#{@alice_report.id}")
+    assert_empty bob_report.mentioning_reports
+    bob_report.save!
+    assert_includes bob_report.mentioning_reports, @alice_report
+  end
 end
